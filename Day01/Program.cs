59d8d93a813994e5610ca0e29855d4db95458642
Karol Berezicki <karolberezicki@gmail.com>
2019-12-01 19:33:16 +0100

    Init, Day01 solution

---
+++ Day01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Day01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = System.IO.File.ReadAllLines("input.txt")
+                .Select(int.Parse)
+                .ToList();
+
+            var part1 = input.Sum(GetFuelRequirement);
+            var part2 = input.Sum(GetFuelRequirementWithRequiredFuel);
+
+            Console.WriteLine($"Part1 {part1}");
+            Console.WriteLine($"Part2 {part2}");
+        }
+
+        public static int GetFuelRequirement(int moduleMass)
+        {
+            return moduleMass / 3 - 2;
+        }
+
+        public static int GetFuelRequirementWithRequiredFuel(int moduleMass)
+        {
+            var fuelRequirement = GetFuelRequirement(moduleMass);
+
+            if (fuelRequirement > 0)
+            {
+                return fuelRequirement + GetFuelRequirementWithRequiredFuel(fuelRequirement);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Day03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Day03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = System.IO.File.ReadAllLines("input.txt")
+                .ToList();
+
+            Console.WriteLine("");
+        }
+    }
+}
