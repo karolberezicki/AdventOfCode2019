3aa79435fdd83e5778661df1463d9a3b4d15e472
Karol Berezicki <karolberezicki@gmail.com>
2019-12-03 20:09:27 +0100

    Day03

---
+++ Day03/Point.cs
@@ -0,0 +1,9 @@
+﻿namespace Day03
+{
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Step { get; set; }
+    }
+}
--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Day03
@@ -8,9 +9,63 @@ namespace Day03
         public static void Main()
         {
             var input = System.IO.File.ReadAllLines("input.txt")
+                .Select(l => l.Trim().Split(",").ToList())
                 .ToList();
 
-            Console.WriteLine("");
+            var wire1 = GetPointsForWire(input[0]);
+            var wire2 = GetPointsForWire(input[1]);
+
+            var intersections = new HashSet<(int X, int Y)>(wire1.Select(p => (p.X, p.Y)).Intersect(wire2.Select(p => (p.X, p.Y))));
+            var closestIntersection = intersections.Min(p => Math.Abs(p.X) + Math.Abs(p.Y));
+
+            var fewestStepsIntersection = intersections
+                .Min(intersection => wire1.First(p => p.X == intersection.X && p.Y == intersection.Y).Step +
+                                     wire2.First(p => p.X == intersection.X && p.Y == intersection.Y).Step);
+
+            Console.WriteLine($"Part1 {closestIntersection}");
+            Console.WriteLine($"Part2 {fewestStepsIntersection}");
+        }
+
+        private static HashSet<Point> GetPointsForWire(List<string> instructions)
+        {
+            var currentPoint = new Point { X = 0, Y = 0 };
+            var wire = new HashSet<Point>();
+
+            foreach (var instruction in instructions)
+            {
+                var direction = instruction[0];
+                var distance = int.Parse(string.Join("", instruction.Skip(1)));
+
+                var move = new List<Point>();
+
+                switch (direction)
+                {
+                    case 'R':
+                        move = Enumerable.Range(1, distance)
+                            .Select(p => new Point { X = currentPoint.X + p, Y = currentPoint.Y, Step = currentPoint.Step + p })
+                            .ToList();
+                        break;
+                    case 'L':
+                        move = Enumerable.Range(1, distance)
+                            .Select(p => new Point { X = currentPoint.X - p, Y = currentPoint.Y, Step = currentPoint.Step + p })
+                            .ToList();
+                        break;
+                    case 'U':
+                        move = Enumerable.Range(1, distance)
+                            .Select(p => new Point { X = currentPoint.X, Y = currentPoint.Y + p, Step = currentPoint.Step + p })
+                            .ToList();
+                        break;
+                    case 'D':
+                        move = Enumerable.Range(1, distance)
+                            .Select(p => new Point { X = currentPoint.X, Y = currentPoint.Y - p, Step = currentPoint.Step + p })
+                            .ToList();
+                        break;
+                }
+
+                currentPoint = move.Last();
+                wire.UnionWith(move);
+            }
+            return wire;
         }
     }
 }
