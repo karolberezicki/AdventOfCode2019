b5a93d71e6a3830e9531f2c3d6af2851b9dbfd90
Karol Berezicki <karolberezicki@gmail.com>
2019-12-07 16:28:15 +0100

    Day07 & Refactor

---
+++ Day07/IntCodeComputer.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day07
+{
+    public class IntCodeComputer
+    {
+        public IntCodeComputer(IEnumerable<int> initMemoryState)
+        {
+            _intCode = initMemoryState.ToList();
+            Inputs = new List<int>();
+            Output = new List<int>();
+        }
+
+        public List<int> Inputs { get; set; }
+        public List<int> Output { get; set; }
+
+        private readonly List<int> _intCode;
+        private int _instructionPointer;
+        private int _inputPointer;
+        public bool IsHalted { get; private set; }
+
+        public void RunIntCode()
+        {
+            while (!IsHalted)
+            {
+                var instruction = _intCode[_instructionPointer].ToString().PadLeft(5, '0');
+                var opCode = GetNumber(instruction[3]) * 10 + GetNumber(instruction[4]);
+                var modeParam1 = GetNumber(instruction[2]);
+                var modeParam2 = GetNumber(instruction[1]);
+
+                switch (opCode)
+                {
+                    case 1: // sum
+                    {
+                        var resultAddress = _intCode[_instructionPointer + 3];
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        var param2 = modeParam2 == 0 ? _intCode[_intCode[_instructionPointer + 2]] : _intCode[_instructionPointer + 2];
+                        _intCode[resultAddress] = param1 + param2;
+                        _instructionPointer += 4;
+                        break;
+                    }
+                    case 2: // multiply
+                    {
+                        var resultAddress = _intCode[_instructionPointer + 3];
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        var param2 = modeParam2 == 0 ? _intCode[_intCode[_instructionPointer + 2]] : _intCode[_instructionPointer + 2];
+                        _intCode[resultAddress] = param1 * param2;
+                        _instructionPointer += 4;
+                        break;
+                    }
+                    case 3: // input
+                    {
+                        var address = _intCode[_instructionPointer + 1];
+                        _intCode[address] = Inputs[_inputPointer];
+                        _inputPointer++;
+                        _instructionPointer += 2;
+                        break;
+                    }
+                    case 4: // output
+                    {
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        Output.Add(param1);
+                        _instructionPointer += 2;
+                        return;
+                    }
+                    case 5: // jump-if-true
+                    {
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        var param2 = modeParam2 == 0 ? _intCode[_intCode[_instructionPointer + 2]] : _intCode[_instructionPointer + 2];
+                        _instructionPointer = param1 != 0 ? param2 : _instructionPointer + 3;
+                        break;
+                    }
+                    case 6: // jump-if-false
+                    {
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        var param2 = modeParam2 == 0 ? _intCode[_intCode[_instructionPointer + 2]] : _intCode[_instructionPointer + 2];
+                        _instructionPointer = param1 == 0 ? param2 : _instructionPointer + 3;
+                        break;
+                    }
+                    case 7: // less than
+                    {
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        var param2 = modeParam2 == 0 ? _intCode[_intCode[_instructionPointer + 2]] : _intCode[_instructionPointer + 2];
+                        var resultAddress = _intCode[_instructionPointer + 3];
+                        _intCode[resultAddress] = param1 < param2 ? 1 : 0;
+                        _instructionPointer += 4;
+                        break;
+                    }
+                    case 8: // equals
+                    {
+
+                        var param1 = modeParam1 == 0 ? _intCode[_intCode[_instructionPointer + 1]] : _intCode[_instructionPointer + 1];
+                        var param2 = modeParam2 == 0 ? _intCode[_intCode[_instructionPointer + 2]] : _intCode[_instructionPointer + 2];
+                        var resultAddress = _intCode[_instructionPointer + 3];
+                        _intCode[resultAddress] = param1 == param2 ? 1 : 0;
+                        _instructionPointer += 4;
+                        break;
+                    }
+                    case 99: // halt
+                        IsHalted = true;
+                        break;
+                }
+            }
+        }
+
+        private static int GetNumber(char character)
+        {
+            return character - '0';
+        }
+    }
+}
--- Day07/Program.cs
+++ Day07/Program.cs
@@ -13,118 +13,69 @@ namespace Day07
                 .Select(int.Parse)
                 .ToList();
 
+            var part1 = Part1(memoryState);
+            var part2 = Part2(memoryState);
+
+            Console.WriteLine($"Part1 {part1}");
+            Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static int Part1(IReadOnlyCollection<int> memoryState)
+        {
             var amplifiers = new List<int> { 0, 1, 2, 3, 4 };
             var amplifiersPermutations = Permutations.GeneratePermutations(amplifiers);
 
             var amplifiersWithSignals = amplifiersPermutations.Select(permutation =>
             {
-                var run4 = RunIntCode(memoryState, permutation[0], 0).First();
-                var run3 = RunIntCode(memoryState, permutation[1], run4).First();
-                var run2 = RunIntCode(memoryState, permutation[2], run3).First();
-                var run1 = RunIntCode(memoryState, permutation[3], run2).First();
-                var run0 = RunIntCode(memoryState, permutation[4], run1).First();
+                var inputValue = 0;
+                foreach (var phaseSetting in permutation)
+                {
+                    var amp = new IntCodeComputer(memoryState);
+                    amp.Inputs.Add(phaseSetting);
+                    amp.Inputs.Add(inputValue);
+                    amp.RunIntCode();
+                    inputValue = amp.Output.Last();
+                }
 
-                return (Amplifier: permutation, Signal: run0);
+                return (Amplifier: permutation, Signal: inputValue);
             });
 
-            var part1 = amplifiersWithSignals.OrderByDescending(t => t.Signal).First().Signal;
-
-            Console.WriteLine($"Part1 {part1}");
-            //Console.WriteLine($"Part2 {part2}");
+            return amplifiersWithSignals.OrderByDescending(t => t.Signal).First().Signal;
         }
 
-        private static List<int> RunIntCode(IEnumerable<int> memoryState, params int[] inputs)
+        private static int Part2(IReadOnlyCollection<int> memoryState)
         {
-            var intCode = memoryState.ToList();
-            var output = new List<int>();
+            var amplifiers = new List<int> { 9, 8, 7, 6, 5 };
+            var amplifiersPermutations = Permutations.GeneratePermutations(amplifiers);
 
-            var instructionPointer = 0;
-            var inputPointer = 0;
+            var amplifiersWithSignals = new List<(List<int> Amplifier, int Signal)>();
 
-            while (true)
+            foreach (var permutation in amplifiersPermutations)
             {
-                var instruction = intCode[instructionPointer].ToString().PadLeft(5, '0');
-                var opCode = GetNumber(instruction[3]) * 10 + GetNumber(instruction[4]);
-                var modeParam1 = GetNumber(instruction[2]);
-                var modeParam2 = GetNumber(instruction[1]);
-
-                switch (opCode)
+                var amps = new List<IntCodeComputer>();
+                foreach (var phaseSetting in permutation)
                 {
-                    case 1: // sum
-                        {
-                            var resultAddress = intCode[instructionPointer + 3];
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            var param2 = modeParam2 == 0 ? intCode[intCode[instructionPointer + 2]] : intCode[instructionPointer + 2];
-                            intCode[resultAddress] = param1 + param2;
-                            instructionPointer += 4;
-                            break;
-                        }
-                    case 2: // multiply
-                        {
-                            var resultAddress = intCode[instructionPointer + 3];
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            var param2 = modeParam2 == 0 ? intCode[intCode[instructionPointer + 2]] : intCode[instructionPointer + 2];
-                            intCode[resultAddress] = param1 * param2;
-                            instructionPointer += 4;
-                            break;
-                        }
-                    case 3: // input
-                        {
-                            var address = intCode[instructionPointer + 1];
-                            intCode[address] = inputs[inputPointer];
-                            inputPointer++;
-                            instructionPointer += 2;
-                            break;
-                        }
-                    case 4: // output
-                        {
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            output.Add(param1);
-                            instructionPointer += 2;
-                            break;
-                        }
-                    case 5: // jump-if-true
-                        {
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            var param2 = modeParam2 == 0 ? intCode[intCode[instructionPointer + 2]] : intCode[instructionPointer + 2];
-                            instructionPointer = param1 != 0 ? param2 : instructionPointer + 3;
-                            break;
-                        }
-                    case 6: // jump-if-false
-                        {
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            var param2 = modeParam2 == 0 ? intCode[intCode[instructionPointer + 2]] : intCode[instructionPointer + 2];
-                            instructionPointer = param1 == 0 ? param2 : instructionPointer + 3;
-                            break;
-                        }
-                    case 7: // less than
-                        {
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            var param2 = modeParam2 == 0 ? intCode[intCode[instructionPointer + 2]] : intCode[instructionPointer + 2];
-                            var resultAddress = intCode[instructionPointer + 3];
-                            intCode[resultAddress] = param1 < param2 ? 1 : 0;
-                            instructionPointer += 4;
-                            break;
-                        }
-                    case 8: // equals
-                        {
-
-                            var param1 = modeParam1 == 0 ? intCode[intCode[instructionPointer + 1]] : intCode[instructionPointer + 1];
-                            var param2 = modeParam2 == 0 ? intCode[intCode[instructionPointer + 2]] : intCode[instructionPointer + 2];
-                            var resultAddress = intCode[instructionPointer + 3];
-                            intCode[resultAddress] = param1 == param2 ? 1 : 0;
-                            instructionPointer += 4;
-                            break;
-                        }
-                    case 99: // halt
-                        return output;
+                    var amp = new IntCodeComputer(memoryState);
+                    amp.Inputs.Add(phaseSetting);
+                    amps.Add(amp);
                 }
-            }
-        }
 
-        private static int GetNumber(char character)
-        {
-            return character - '0';
+                var inputValue = 0;
+
+                while (!amps.Last().IsHalted)
+                {
+                    foreach (var amp in amps)
+                    {
+                        amp.Inputs.Add(inputValue);
+                        amp.RunIntCode();
+                        inputValue = amp.Output.Last();
+                    }
+                }
+
+                amplifiersWithSignals.Add((Amplifier: permutation, Signal: inputValue));
+            }
+
+            return amplifiersWithSignals.OrderByDescending(t => t.Signal).First().Signal;
         }
     }
 }
