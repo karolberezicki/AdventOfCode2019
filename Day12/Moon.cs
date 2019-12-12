1611b26d0c72f1898f87414cd9ff582ef9b233e8
Karol Berezicki <karolberezicki@gmail.com>
2019-12-12 08:10:12 +0100

    Day12, Part 1

---
+++ Day12/Moon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Day12
+{
+    [DebuggerDisplay("pos=<x={X}, y= {Y}, z= {Z}>, vel=<x= {Vx}, y= {Vy}, z= {Vz}>")]
+    public class Moon
+    {
+        public Moon(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public int Vx { get; set; }
+        public int Vy { get; set; }
+        public int Vz { get; set; }
+
+        public void ApplyGravity(Moon moon)
+        {
+            if (X < moon.X)
+            {
+                Vx += 1;
+                moon.Vx -= 1;
+            }
+
+            if (X > moon.X)
+            {
+                Vx -= 1;
+                moon.Vx += 1;
+            }
+
+            if (Y < moon.Y)
+            {
+                Vy += 1;
+                moon.Vy -= 1;
+            }
+
+            if (Y > moon.Y)
+            {
+                Vy -= 1;
+                moon.Vy += 1;
+            }
+
+            if (Z < moon.Z)
+            {
+                Vz += 1;
+                moon.Vz -= 1;
+            }
+
+            if (Z > moon.Z)
+            {
+                Vz -= 1;
+                moon.Vz += 1;
+            }
+        }
+
+        public void ApplyVelocity()
+        {
+            X += Vx;
+            Y += Vy;
+            Z += Vz;
+        }
+
+        public int PotentialEnergy => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+        public int KineticEnergy => Math.Abs(Vx) + Math.Abs(Vy) + Math.Abs(Vz);
+        public int TotalEnergy => PotentialEnergy * KineticEnergy;
+    }
+}
--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day12
 {
@@ -7,10 +9,33 @@ namespace Day12
     {
         public static void Main()
         {
-            var input = System.IO.File.ReadAllLines("input.txt")
+            var regex = new Regex(@"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>");
+            var moons = File.ReadAllLines("input.txt")
+                .Select(l => regex.Match(l).Groups.Values.Select(v => v.Value).ToArray())
+                .Select(l => new Moon(int.Parse(l[1]), int.Parse(l[2]), int.Parse(l[3])))
                 .ToList();
 
-            Console.WriteLine("");
+            const int stepsCount = 1000;
+
+            for (var step = 0; step < stepsCount; step++)
+            {
+                for (var i = 0; i < moons.Count; i++)
+                {
+                    for (var j = i + 1; j < moons.Count; j++)
+                    {
+                        moons[i].ApplyGravity(moons[j]);
+                    }
+                }
+
+                foreach (var moon in moons)
+                {
+                    moon.ApplyVelocity();
+                }
+            }
+
+            var part1 = moons.Select(m => m.TotalEnergy).Sum();
+
+            Console.WriteLine($"Part1 {part1}");
         }
     }
 }
