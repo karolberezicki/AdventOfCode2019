baa1e8778e0a5467209056718328c731e7e13353
Karol Berezicki <karolberezicki@gmail.com>
2019-12-15 03:48:02 +0100

    Day14

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,16 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Day14
 {
     public class Program
     {
+        private static Dictionary<string, Recipe> _recipes;
+        private const long CargoHold = 1000000000000L;
+
         public static void Main()
         {
             var input = System.IO.File.ReadAllLines("input.txt")
                 .ToList();
 
-            Console.WriteLine("");
+            _recipes = input
+                .Select(line => new Recipe(line))
+                .ToDictionary(r => r.OutputChemical, r => r);
+
+            var part1 = FindRequiredOre(1);
+            var part2 = CargoHold / part1;
+            var usedOre = 0L;
+            while (usedOre <= CargoHold)
+            {
+                usedOre = FindRequiredOre(part2 + 1);
+                part2 = Math.Max(part2, part2 * CargoHold / usedOre);
+            }
+
+            Console.WriteLine($"Part1 {part1}");
+            Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static long FindRequiredOre(long fuelAmount)
+        {
+            var requiredChemicals = _recipes
+                .Select(r => r.Value.OutputChemical)
+                .ToDictionary(k => k, v => 0L);
+
+            requiredChemicals["FUEL"] = fuelAmount;
+            requiredChemicals["ORE"] = 0;
+
+            while (requiredChemicals.Any(chemical => chemical.Key != "ORE" && chemical.Value > 0))
+            {
+                var requiredChemical = requiredChemicals.First(chemical => chemical.Key != "ORE" && chemical.Value > 0).Key;
+                var requiredAmount = requiredChemicals[requiredChemical];
+                var multiplier = Math.Max(requiredAmount / _recipes[requiredChemical].OutputCount, 1);
+
+                requiredChemicals[requiredChemical] -= _recipes[requiredChemical].OutputCount * multiplier;
+                foreach (var (ingredientName, recipeAmount) in _recipes[requiredChemical].Ingredients)
+                {
+                    requiredChemicals[ingredientName] += recipeAmount * multiplier;
+                }
+            }
+
+            var requiredOre = requiredChemicals["ORE"];
+            return requiredOre;
         }
     }
 }
---
+++ Day14/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Day14
+{
+    [DebuggerDisplay("{" + nameof(RecipeDescription) + ",nq}")]
+    public class Recipe
+    {
+        public string RecipeDescription { get; set; }
+        public string OutputChemical { get; }
+        public long OutputCount { get; }
+        public Dictionary<string, long> Ingredients { get; set; }
+
+        public Recipe(string recipeDescription)
+        {
+            RecipeDescription = recipeDescription;
+            var recipe = RecipeDescription.Split(new[] { " => " }, StringSplitOptions.RemoveEmptyEntries);
+            var product = recipe[1].Split(" ");
+            OutputChemical = product[1].Trim();
+            OutputCount = long.Parse(product[0].Trim());
+            Ingredients = new Dictionary<string, long>();
+            foreach (var ingredientDescription in recipe[0].Split(",", StringSplitOptions.RemoveEmptyEntries))
+            {
+                var ingredient = ingredientDescription.Trim().Split(" ");
+                Ingredients.Add(ingredient[1].Trim(), long.Parse(ingredient[0].Trim()));
+            }
+        }
+    }
+}
