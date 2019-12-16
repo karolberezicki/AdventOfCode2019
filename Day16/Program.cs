07c2387c9f6ac90c965afecda9772a9e613e1a77
Karol Berezicki <karolberezicki@gmail.com>
2019-12-16 20:53:18 +0100

    Day16

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -15,9 +15,18 @@ namespace Day16
             signal = FlawedFrequencyTransmission(signal);
 
             var part1 = string.Join("", signal.Take(8));
-
             Console.WriteLine($"Part1 {part1}");
-            //Console.WriteLine($"Part2 {part2}");
+
+            var signal2 = input
+                .Trim().Select(c => c - '0').ToList();
+
+            signal2 = Enumerable.Repeat(signal2, 10000).SelectMany(n => n).ToList();
+            var messageOffset = int.Parse(string.Join("", input.Take(7)));
+
+            signal2 = FlawedFrequencyTransmission2(signal2, messageOffset);
+
+            var part2 = string.Join("", signal2.Take(8));
+            Console.WriteLine($"Part2 {part2}");
         }
 
         private static List<int> FlawedFrequencyTransmission(List<int> signal)
@@ -54,6 +63,24 @@ namespace Day16
             }
 
             return signal;
+        }
+
+        private static List<int> FlawedFrequencyTransmission2(List<int> signal, int messageOffset)
+        {
+            var index = 0;
+            var signalDict = signal.Skip(messageOffset)
+                .ToDictionary(item => index++);
+
+            for (var phase = 1; phase <= 100; phase++)
+            {
+                for (var i = signalDict.Count - 1; i >= 0; i--)
+                {
+                    var previousDigit = signalDict.ContainsKey(i + 1) ? signalDict[i + 1] : 0;
+                    signalDict[i] = Math.Abs(previousDigit + signalDict[i]) % 10;
+                }
+            }
+
+            return signalDict.Values.ToList();
         }
     }
 }
