8b079936f7a5938397980f34a30ba4d666acd67b
karolberezicki <karolberezicki@gmail.com>
2019-12-26 20:50:14 +0100

    Day25

--- Day25/Program.cs
+++ Day25/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntCode;
+using System;
 using System.Linq;
 
 namespace Day25
@@ -7,10 +8,53 @@ namespace Day25
     {
         public static void Main()
         {
-            var input = System.IO.File.ReadAllLines("input.txt")
+            // Solution items: hologram, jam, semiconductor, mutex
+            // Additional items: polygon, prime number, monolith, weather machine
+            // Unsafe items: infinite loop, photons, giant electromagnet, escape pod, molten lava
+            // TODO: Write an automatic solution
+
+            var input = System.IO.File.ReadAllText("input.txt");
+
+            var memoryState = input.Split(",")
+                .Select(long.Parse)
                 .ToList();
 
-            Console.WriteLine("");
+            var icc = new IntCodeComputer(memoryState);
+
+            Console.Clear();
+
+            while (!icc.IsHalted)
+            {
+                icc.RunIntCode(BreakMode.Input);
+
+                PrintIntCodeMessage(icc);
+                var inputMessage = Console.ReadLine() ?? string.Empty;
+
+                inputMessage = inputMessage switch
+                {
+                    "n" => "north",
+                    "s" => "south",
+                    "w" => "west",
+                    "e" => "east",
+                    "i" => "inv",
+                    _ => inputMessage
+                };
+
+                foreach (var character in inputMessage)
+                {
+                    icc.Inputs.Enqueue(character);
+                }
+                icc.Inputs.Enqueue(10);
+            }
+
+            PrintIntCodeMessage(icc);
+        }
+
+        private static void PrintIntCodeMessage(IntCodeComputer icc)
+        {
+            var outputMessage = new string(icc.Output.Select(c => (char) c).ToArray());
+            icc.Output.Clear();
+            Console.WriteLine(outputMessage);
         }
     }
 }
