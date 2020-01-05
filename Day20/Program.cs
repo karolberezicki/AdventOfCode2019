d35e838923759e8bb43ed02678dfc1701a1ac180
Karol Berezicki <karolberezicki@gmail.com>
2020-01-05 17:53:29 +0100

    Day20

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -13,30 +13,149 @@ namespace Day20
 
         public static void Main()
         {
-            var input = System.IO.File.ReadAllLines("test2.txt")
+            var input = System.IO.File.ReadAllLines("input.txt")
                 .ToList();
 
             GenerateMaze(input);
             GenerateLabels();
             GeneratePortals();
 
-            var part1 = Part1();
+            var start = _labels["AA"].First();
+            var target = _labels["ZZ"].First();
+
+            var part1 = Part1(start, target);
             Console.WriteLine($"Part1 {part1}");
+
+            var part2 = Part2(start, target);
+            Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static int Part1((int X, int Y) start, (int X, int Y) target)
+        {
+            var statesQueue = new Queue<(int X, int Y, int Distance)>(new[] { (start.X, start.Y, 0) });
+            (int X, int Y, int Distance) finishState = (0, 0, int.MaxValue);
+            var visited = new HashSet<(int X, int Y)> { start };
+
+            while (statesQueue.Count > 0)
+            {
+                var currentState = statesQueue.Dequeue();
+
+                if (currentState.X == target.X && currentState.Y == target.Y && currentState.Distance < finishState.Distance)
+                {
+                    finishState = currentState;
+                }
+                else
+                {
+                    var possibleMoves = new HashSet<(int X, int Y, int Distance)>
+                    {
+                        (currentState.X + 1, currentState.Y, currentState.Distance + 1),
+                        (currentState.X - 1, currentState.Y, currentState.Distance + 1),
+                        (currentState.X, currentState.Y + 1, currentState.Distance + 1),
+                        (currentState.X, currentState.Y - 1, currentState.Distance + 1)
+                    };
+
+                    if (_portals.ContainsKey((currentState.X, currentState.Y)))
+                    {
+                        var (portalDestinationX, portalDestinationY) = _portals[(currentState.X, currentState.Y)];
+                        possibleMoves.Add((portalDestinationX, portalDestinationY, currentState.Distance + 1));
+                    }
+
+                    if (finishState != default)
+                    {
+                        possibleMoves =
+                            new HashSet<(int X, int Y, int Distance)>(possibleMoves.Where(move =>
+                                move.Distance < finishState.Distance));
+                    }
+
+                    foreach (var newState in possibleMoves)
+                    {
+                        var position = (newState.X, newState.Y);
+                        var toExplore = _maze.ContainsKey(position)
+                                          && _maze[position] == '.'
+                                          && !visited.Contains(position)
+                                          && !statesQueue.Contains(newState);
+                        if (toExplore)
+                        {
+                            visited.Add(position);
+                            statesQueue.Enqueue(newState);
+                        }
+                    }
+                }
+            }
+
+            return finishState.Distance;
+        }
+
+        private static int Part2((int X, int Y) start, (int X, int Y) target)
+        {
+            var statesQueue = new Queue<(int X, int Y, int Distance, int Level)>(new[] { (start.X, start.Y, 0, 0) });
+            (int X, int Y, int Distance, int Level) finishState = (0, 0, int.MaxValue, 0);
+            var visited = new HashSet<(int X, int Y, int Level)> { (start.X, start.Y, 0) };
+
+            while (statesQueue.Count > 0)
+            {
+                var currentState = statesQueue.Dequeue();
+
+                if (currentState.X == target.X && currentState.Y == target.Y && currentState.Distance < finishState.Distance && currentState.Level == 0)
+                {
+                    finishState = currentState;
+                }
+                else
+                {
+                    var possibleMoves = new HashSet<(int X, int Y, int Distance, int Level)>
+                    {
+                        (currentState.X + 1, currentState.Y, currentState.Distance + 1, currentState.Level),
+                        (currentState.X - 1, currentState.Y, currentState.Distance + 1, currentState.Level),
+                        (currentState.X, currentState.Y + 1, currentState.Distance + 1, currentState.Level),
+                        (currentState.X, currentState.Y - 1, currentState.Distance + 1, currentState.Level)
+                    };
+
+                    if (_portals.ContainsKey((currentState.X, currentState.Y)))
+                    {
+                        var (portalDestinationX, portalDestinationY) = _portals[(currentState.X, currentState.Y)];
+                        var portalLevel = IsOuterEdge((currentState.X, currentState.Y))
+                            ? currentState.Level - 1
+                            : currentState.Level + 1;
+
+                        if (portalLevel >= 0)
+                        {
+                            possibleMoves.Add((portalDestinationX, portalDestinationY, currentState.Distance + 1, portalLevel));
+                        }
+                    }
+
+                    if (finishState != default)
+                    {
+                        possibleMoves =
+                            new HashSet<(int X, int Y, int Distance, int Level)>(possibleMoves.Where(move =>
+                                move.Distance < finishState.Distance));
+                    }
+
+                    foreach (var newState in possibleMoves)
+                    {
+                        var positionCoordinates = (newState.X, newState.Y);
+                        var position = (newState.X, newState.Y, newState.Level);
+                        var toExplore = _maze.ContainsKey(positionCoordinates)
+                                          && _maze[positionCoordinates] == '.'
+                                          && !visited.Contains(position)
+                                          && !statesQueue.Contains(newState);
+                        if (toExplore)
+                        {
+                            visited.Add(position);
+                            statesQueue.Enqueue(newState);
+                        }
+                    }
+                }
+            }
+
+            return finishState.Distance;
         }
 
         private static bool IsOuterEdge((int X, int Y) coordinate)
         {
             return _boundaries.XMin == coordinate.X
                    || _boundaries.XMax == coordinate.X
-                   || _boundaries.YMax == coordinate.Y
+                   || _boundaries.YMin == coordinate.Y
                    || _boundaries.YMax == coordinate.Y;
-        }
-
-        private static int Part1()
-        {
-            var start = _labels["AA"].First();
-            var target = _labels["ZZ"].First();
-            return FindShortestDistanceThroughPortals(start, target);
         }
 
         private static void GeneratePortals()
@@ -92,111 +211,7 @@ namespace Day20
             }
         }
 
-        private static int FindShortestDistance((int X, int Y) start, (int X, int Y) target)
-        {
-            var statesQueue = new Queue<(int X, int Y, int Distance)>(new[] { (start.X, start.Y, 0) });
-            (int X, int Y, int Distance) finishState = (0, 0, int.MaxValue);
-            var visited = new HashSet<(int X, int Y)> { start };
 
-            while (statesQueue.Count > 0)
-            {
-                var currentState = statesQueue.Dequeue();
-
-                if (currentState.X == target.X && currentState.Y == target.Y && currentState.Distance < finishState.Distance)
-                {
-                    finishState = currentState;
-                }
-                else
-                {
-                    var possibleMoves = new HashSet<(int X, int Y, int Distance)>
-                    {
-                        (currentState.X + 1, currentState.Y, currentState.Distance + 1),
-                        (currentState.X - 1, currentState.Y, currentState.Distance + 1),
-                        (currentState.X, currentState.Y + 1, currentState.Distance + 1),
-                        (currentState.X, currentState.Y - 1, currentState.Distance + 1)
-                    };
-
-                    if (finishState != default)
-                    {
-                        possibleMoves =
-                            new HashSet<(int X, int Y, int Distance)>(possibleMoves.Where(move =>
-                                move.Distance < finishState.Distance));
-                    }
-
-                    foreach (var newState in possibleMoves)
-                    {
-                        var position = (newState.X, newState.Y);
-                        var toExplore = _maze.ContainsKey(position)
-                                          && _maze[position] == '.'
-                                          && !visited.Contains(position)
-                                          && !statesQueue.Contains(newState);
-                        if (toExplore)
-                        {
-                            visited.Add(position);
-                            statesQueue.Enqueue(newState);
-                        }
-                    }
-                }
-            }
-
-            return finishState.Distance;
-        }
-
-        private static int FindShortestDistanceThroughPortals((int X, int Y) start, (int X, int Y) target)
-        {
-            var statesQueue = new Queue<(int X, int Y, int Distance)>(new[] { (start.X, start.Y, 0) });
-            (int X, int Y, int Distance) finishState = (0, 0, int.MaxValue);
-            var visited = new HashSet<(int X, int Y)> { start };
-
-            while (statesQueue.Count > 0)
-            {
-                var currentState = statesQueue.Dequeue();
-
-                if (currentState.X == target.X && currentState.Y == target.Y && currentState.Distance < finishState.Distance)
-                {
-                    finishState = currentState;
-                }
-                else
-                {
-                    var possibleMoves = new HashSet<(int X, int Y, int Distance)>
-                    {
-                        (currentState.X + 1, currentState.Y, currentState.Distance + 1),
-                        (currentState.X - 1, currentState.Y, currentState.Distance + 1),
-                        (currentState.X, currentState.Y + 1, currentState.Distance + 1),
-                        (currentState.X, currentState.Y - 1, currentState.Distance + 1)
-                    };
-
-                    if (_portals.ContainsKey((currentState.X, currentState.Y)))
-                    {
-                        var (portalDestinationX, portalDestinationY) = _portals[(currentState.X, currentState.Y)];
-                        possibleMoves.Add((portalDestinationX, portalDestinationY, currentState.Distance + 1));
-                    }
-
-                    if (finishState != default)
-                    {
-                        possibleMoves =
-                            new HashSet<(int X, int Y, int Distance)>(possibleMoves.Where(move =>
-                                move.Distance < finishState.Distance));
-                    }
-
-                    foreach (var newState in possibleMoves)
-                    {
-                        var position = (newState.X, newState.Y);
-                        var toExplore = _maze.ContainsKey(position)
-                                          && _maze[position] == '.'
-                                          && !visited.Contains(position)
-                                          && !statesQueue.Contains(newState);
-                        if (toExplore)
-                        {
-                            visited.Add(position);
-                            statesQueue.Enqueue(newState);
-                        }
-                    }
-                }
-            }
-
-            return finishState.Distance;
-        }
 
         private static void GenerateMaze(List<string> input)
         {
